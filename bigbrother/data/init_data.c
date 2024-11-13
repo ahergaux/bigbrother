af31c031b63d1dc76676d71d66ca30f328c0112d
Alexandre Hergaux <alexandrehergaux@eduroam-prg-og-1-29-3.net.univ-paris-diderot.fr>
2024-11-13 14:17:41 +0100

    test

---
+++ bigbrother/data/init_data.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char prenom[] = {};
+char nom[] = {};
+char genre[] = {};
+char ville[] = {};
+char pays[] = {};
+char nationalite[]={};
+ 
+int main(){
+
+}
